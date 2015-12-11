ecfcbfa745511947256bd239dee7566002d40e42
Andrew Kubera <andrewkubera@gmail.com>
2015-12-10 21:34:17 -0500

    Fit script now actually does fits and presents user with qinv plots.

--- post_analysis/3d_fit.py
+++ post_analysis/3d_fit.py
@@ -10,12 +10,33 @@ from collections import defaultdict
 import types
 import numbers
 import ctypes
+import ROOT
 from ROOT import (
     TFile,
     TMinuit,
     TApplication,
     TObjString,
+    TGraph,
 )
+from lmfit import minimize, Parameters, report_fit
+
+CACHE = []
+STORED_CANVASES = []
+
+
+def gen_canvas():
+    """
+    Create a new canvas, cached so it wont go out of scope.
+    """
+    global STORED_CANVASES
+    name = "c%0.2x" % len(STORED_CANVASES)
+    # print("Creating Canvas %s" % name)
+    canvas = ROOT.TCanvas(name, "Canvas", 200, 10, 700, 500)
+    STORED_CANVASES.append(canvas)
+    return canvas
+
+# gen_canvas = iter(gen_canvas())
+
 
 def get_root_object(obj, paths):
     if isinstance(paths, (list, tuple)):
@@ -36,7 +57,14 @@ def get_root_object(obj, paths):
     else:
         return get_root_object(new_obj, rest[0])
 
-def get_ratio(name, num, den, norm_x_ranges=(0.4, 0.7)):
+
+def hist_method_to_array(hist, func):
+    nbins = hist.GetNbinsX()
+    func = getattr(hist, func)
+    return np.array(list(map(func, range(1, nbins))))
+
+
+def get_ratio(name, num, den, norm_x_ranges, *, cache=True):
     """
     Returns the normalized ratio of the two histograms
     """
@@ -52,6 +80,10 @@ def get_ratio(name, num, den, norm_x_ranges=(0.4, 0.7)):
     ratio = num.Clone(name)
     ratio.Divide(num, den, 1.0 / num_scale, 1.0 / den_scale)
     ratio.SetStats(False)
+
+    if cache:
+        CACHE.append(ratio)
+
     return ratio
 
 
@@ -90,6 +122,8 @@ for analysis in femtolist:
 
     def tree(): return defaultdict(tree)
     analysis_meta = tree()
+
+    print("********* %s **********" % analysis_name)
 
     # get settings
     for s in str(settings).split("\n")[1:-1]:
@@ -131,8 +165,7 @@ for analysis in femtolist:
     num_scale = 1.0 / num.Integral(*num_norm_bin_range)
     den_scale = 1.0 / den.Integral(*den_norm_bin_range)
 
-    ratio = num.Clone("ratio")
-    ratio.Divide(num, den, num_scale, den_scale)
+    ratio = get_ratio('ratio', num, den, (0.4, 0.7))
     ratio.SetTitle(make_cf_title(title='Q_{inv}', units='q_{inv}'))
     ratio.Write()
 
@@ -146,24 +179,102 @@ for analysis in femtolist:
     num_qout = q3d_num.ProjectionX("num_qout")
     den_qout = q3d_den.ProjectionX("den_qout")
 
-    ratio_qo = get_ratio('ratio_qo', num_qout, den_qout, (0.4, 0.7))
+    ratio_qo = get_ratio('ratio_qo', num_qout, den_qout, [(-0.4, -0.7), (0.4, 0.7)])
     ratio_qo.SetTitle(make_cf_title(title='Q_{out}', units='q_{out}'))
     ratio_qo.Write()
 
     num_qside = q3d_num.ProjectionY("num_qside")
     den_qside = q3d_den.ProjectionY("den_qside")
 
-    ratio_qs = get_ratio('ratio_qs', num_qside, den_qside, (0.4, 0.7))
+    ratio_qs = get_ratio('ratio_qs', num_qside, den_qside, [(-0.4, -0.7), (0.4, 0.7)])
     ratio_qs.SetTitle(make_cf_title(title='Q_{side}', units='q_{side}'))
     ratio_qs.Write()
 
     num_qlong = q3d_num.ProjectionZ("num_qlong")
     den_qlong = q3d_den.ProjectionZ("den_qlong")
 
-    ratio_ql = get_ratio('ratio_ql', num_qlong, den_qlong, (0.4, 0.7))
+    ratio_ql = get_ratio('ratio_ql', num_qlong, den_qlong, [(-0.4, -0.7), (0.4, 0.7)])
     ratio_ql.SetTitle(make_cf_title(title='Q_{long}', units='q_{long}'))
     ratio_ql.Write()
 
+
+    #
+    # Do Fit
+    #
+
+    X = hist_method_to_array(ratio, 'GetBinCenter')
+    Y = hist_method_to_array(ratio, 'GetBinContent')
+    E = hist_method_to_array(ratio, 'GetBinError')
+
+    stop_fit = ratio.FindBin(0.25)
+
+    qinv_params = Parameters()
+    qinv_params.add('radius', value=0.5, min=0.0)
+    qinv_params.add('lam', value=0.5)  # , min=0.0, max=1.0)
+
+    def fit_qinv(params, x, data=None):
+        model = 1 + params['lam'] * np.exp(list(-(x * params['radius']) ** 2))
+        if data is None:
+            return model
+
+        shape = np.shape(data)
+        func_count = shape[0] / 2
+
+        if func_count % 1:
+            raise ValueError("Data shape not multiple of 2. Mismatched data and errors")
+
+        # resid = sum(np.sqrt((data[i] - model) ** 2 / data[i + 1] ** 2)
+        #             for i in range(0, shape[0], 2))
+
+        resid = model - data[0]
+        res = np.sqrt( resid ** 2 / data[1] ** 2 )
+        return res
+
+    x_vals = X[:stop_fit]
+    y_vals = Y[:stop_fit]
+    e_vals = E[:stop_fit]
+
+    qinv_fit = minimize(fit_qinv, qinv_params, args=(x_vals, (y_vals, e_vals)))
+    report_fit(qinv_fit)
+    FIT_Y = fit_qinv(qinv_fit.params, X)
+
+    canvas_qinv = gen_canvas()
+    # canvas_qinv.cd(1)
+
+    fit_plot = TGraph(len(X), X, FIT_Y)
+    CACHE.append(fit_plot)
+
+    ratio.Draw()
+    fit_plot.SetLineColor(2)
+    fit_plot.Draw("same")
+    canvas_qinv.Draw()
+
+    continue
+
+    #
+    #
+    #
+
+    def split_hist(hist):
+        return (
+            hist_method_to_array(hist, 'GetBinCenter'),
+            hist_method_to_array(hist, 'GetBinContent'),
+            hist_method_to_array(hist, 'GetBinError'),
+        )
+
+    Xo, Yo, Eo = split_hist(ratio_qo)
+    Xs, Ys, Es = split_hist(ratio_qs)
+    Xl, Yl, El = split_hist(ratio_ql)
+
+    qinv_params = Parameters()
+    qinv_params.add('r_out', value=0.5, min=0.0)
+    qinv_params.add('r_side', value=0.5, min=0.0)
+    qinv_params.add('r_long', value=0.5, min=0.0)
+    qinv_params.add('lam', value=0.5, min=0.0, max=1.0)
+
+
+
+input()
 
     # ratio.Draw()
     # break
