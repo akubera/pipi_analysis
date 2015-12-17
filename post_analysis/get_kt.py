7a1936c835d20d6e595fb2ac0a3bfe3a71dd46be
Andrew Kubera <andrewkubera@gmail.com>
2015-12-16 21:35:06 -0500

    Added script which outputs the passing pion kt distributions found in each of the analyses

---
+++ post_analysis/get_kt.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+#
+# post_analysis/get_kt.py
+#
+
+import sys
+import math
+import time
+import numpy as np
+from argparse import ArgumentParser
+from itertools import starmap as apply
+from functools import partial
+from collections import defaultdict
+import numbers
+from fit_params import (
+    FitParam,
+    fit_params,
+)
+from lmfit import minimize, Parameters, report_fit
+import ROOT
+from ROOT import (
+    TFile,
+    TMinuit,
+)
+import pionpion
+from pionpion.root_helpers import get_root_object
+
+
+parser = ArgumentParser()
+parser.add_argument("filename", help="root filename to analyze")
+parser.add_argument("output",
+                    nargs='?',
+                    default='Result.root',
+                    help="root filename to analyze")
+args = parser.parse_args()
+
+file = TFile(args.filename, 'READ')
+
+femtolist = pionpion.Femtolist(file)
+
+output = TFile(args.output, 'RECREATE')
+
+
+for analysis in femtolist:
+
+    analysis_name = analysis.GetName()
+
+    output.mkdir(analysis_name)
+    output.cd(analysis_name)
+
+    kt = get_root_object(analysis, "Pair.pass.kt")
+    if kt == None:
+        continue
+    kt.Write()
+
+output.Write()
+output.Close()
