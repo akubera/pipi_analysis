fe861368a93ef9c07e4c48d4ce9f72a0bd963437
Andrew Kubera <andrewkubera@gmail.com>
2016-01-06 22:10:06 -0500

    3d_fit: Reorganized code so qinv runs and then creates a q3d study

--- post_analysis/3d_fit.py
+++ post_analysis/3d_fit.py
@@ -12,7 +12,10 @@ import numbers
 import ctypes
 import time
 from pionpion.analysis import Analysis
-
+from pionpion.q3d import Q3D
+from pionpion.fit import (
+    fitfunc_qinv,
+)
 
 TSTART = time.monotonic()
 print("loading ROOT... ", end='', flush=True, file=sys.stderr)
@@ -116,8 +119,68 @@ if femtolist == None:
 output = TFile(args.output, 'RECREATE')
 
 # analysis = get_root_object(femtolist, "PiPiAnalysis_00_10_pip")
+TRACK_CUT_INFO_STR = """\
+ Charge: {charge}
+ Mass: {mass}
+ Pt: {pt[minimum]} -> {pt[maximum]}
+"""
 
 
+def do_qinv_analysis(num, den, cf_title="CF; q (GeV); CF(q)"):
+    """
+    Code performing the qinv analysis - should probably be moved to a submodule
+    """
+
+    norm_x_range = 0.4, 0.7
+    num_norm_bin_range = map(num.FindBin, norm_x_range)
+    den_norm_bin_range = map(num.FindBin, norm_x_range)
+
+    num_scale = 1.0 / num.Integral(*num_norm_bin_range)
+    den_scale = 1.0 / den.Integral(*den_norm_bin_range)
+
+    ratio = get_ratio('ratio', num, den, (0.4, 0.7))
+    ratio.SetTitle(cf_title)
+    ratio.Write()
+
+    #
+    # Do Fit
+    #
+
+    X = hist_method_to_array(ratio, 'GetBinCenter')
+    Y = hist_method_to_array(ratio, 'GetBinContent')
+    E = hist_method_to_array(ratio, 'GetBinError')
+
+    stop_fit = ratio.FindBin(0.25)
+
+    qinv_params = Parameters()
+    qinv_params.add('radius', value=0.5, min=0.0)
+    qinv_params.add('lam', value=0.5)  # , min=0.0, max=1.0)
+
+    x_vals = X[:stop_fit]
+    y_vals = Y[:stop_fit]
+    e_vals = E[:stop_fit]
+
+    data = (y_vals, e_vals)
+
+    qinv_fit = minimize(fitfunc_qinv, qinv_params, args=(x_vals, data))
+    report_fit(qinv_fit)
+
+    FIT_X = np.linspace(X[0], X[-1], 300)
+    FIT_Y = fitfunc_qinv(qinv_fit.params, FIT_X)
+
+    canvas_qinv = gen_canvas()
+    # canvas_qinv.cd(1)
+
+    fit_plot = TGraph(len(FIT_X), FIT_X, FIT_Y)
+    CACHE.append(fit_plot)
+
+    ratio.Draw()
+    fit_plot.SetLineColor(2)
+    fit_plot.Draw("same")
+    canvas_qinv.Draw()
+
+
+# Loop over analyses
 for analysis in femtolist:
 
     analysis_name = analysis.GetName()
@@ -133,35 +196,42 @@ for analysis in femtolist:
         continue
 
     track_meta = analysis_meta['AliFemtoSimpleAnalysis']['AliFemtoESDTrackCut']
-    print(" Charge:", track_meta['charge'])
-    print(" Mass:", track_meta['mass'])
-    print(" Pt:", track_meta['pt']['minimum'], '->', track_meta['pt']['maximum'])
+    print(TRACK_CUT_INFO_STR.format(**track_meta))
+
+    charge = int(track_meta['charge'])
+    chstr = '-' if charge < 0 else '+' if charge > 0 else '0'
+    cf_title = "#pi^{{ %s }} {title} CF; {units} (GeV); C({units})" % (chstr)
+    make_cf_title = cf_title.format
+
+    #
+    # Qinv
+    #
 
     num = get_root_object(analysis, ["Numc_qinv_pip", "Numc_qinv_pim"])
     den = get_root_object(analysis, ["Denc_qinv_pip", "Denc_qinv_pim"])
 
     if num == None or den == None:
-        print("Bad analysis", analysis_name)
+        print("Missing Qinv plots")
         print('  num:', num)
         print('  den:', den)
-        continue
+    else:
+        print(" ***** Q_inv Study *****\n")
+        qinv_title = make_cf_title(title='Q_{inv}', units='q_{inv}')
+        do_qinv_analysis(num, den, cf_title=qinv_title)
 
-    charge = int(track_meta['charge'])
-    chstr = '-' if charge < 0 else '+' if charge > 0 else '0'
+    #
+    # Qinv - Kt binned
+    #
+    kt_qinv = get_root_object(analysis, ["KT_Qinv"])
+    if kt_qinv != None:
+        for ktbin in kt_qinv:
+            ktn = get_root_object(ktbin, ["Numc_qinv_pip", "Numc_qinv_pim"])
+            ktd = get_root_object(ktbin, ["Denc_qinv_pip", "Denc_qinv_pim"])
 
-    cf_title = "#pi^{{ %s }} {title} CF; {units} (GeV); C({units})" % (chstr)
-    make_cf_title = cf_title.format
 
-    norm_x_range = 0.4, 0.7
-    num_norm_bin_range = map(num.FindBin, norm_x_range)
-    den_norm_bin_range = map(num.FindBin, norm_x_range)
-
-    num_scale = 1.0 / num.Integral(*num_norm_bin_range)
-    den_scale = 1.0 / den.Integral(*den_norm_bin_range)
-
-    ratio = get_ratio('ratio', num, den, (0.4, 0.7))
-    ratio.SetTitle(make_cf_title(title='Q_{inv}', units='q_{inv}'))
-    ratio.Write()
+    #
+    # 3D
+    #
 
     q3d_num = get_root_object(analysis, ["Num_q3D_pip", "Num_q3D_pim"])
     q3d_den = get_root_object(analysis, ["Den_q3D_pip", "Den_q3D_pim"])
@@ -170,32 +240,15 @@ for analysis in femtolist:
         print("No 3D histogram found in", analysis_name)
         continue
 
-    from pionpion.q3d import Q3D
+    print(" ***** 3D Study Q{osl} *****\n")
 
-    num_hist_3d = Q3D(q3d_num)
-    den_hist_3d = Q3D(q3d_den)
-
-
-    print('   ', num_hist_3d[-.1:.1, -.1:.1, 0.0])
-    # l = num_hist_3d.bin_at(slice(None), 0.1, 0.2)
-    # print('   ', num_hist_3d.value_at(0.1, 0.1, 0.2))
-    # print('   ', num_hist_3d.value_in(*l))
-    # # print("   ", we[0.0, -0.1:0.5, 0.0])
-    # bins = tuple(axis.FindBin(0.0) for axis in we._axes)
-    # print("   ", bins)
-    # print("   ", we._ptr.GetBinContent(*bins))
-    # print("   ", we.error[10, 10, 10])
-    # print("  >", we._ptr.GetBinError(10, 10, 10))
+    hist_3d = Q3D(q3d_num, q3d_den)
 
     y_dom = (-0.01, 0.01)
-    z_dom = (-0.00, 0.00)
-    q_out_num = num_hist_3d.projection_out(y_domain=y_dom, z_domain=z_dom)
-    q_out_num_new = num_hist_3d.project_1d(0, y_dom, z_dom)
+    z_dom = (-0.01, 0.01)
+    q_out_num = hist_3d.num.project_1d(0, y_dom, z_dom)
     print(np.shape(q_out_num))
-    print(np.shape(q_out_num_new))
-    # print(q_out_num)
-    # print(q_out_num_new)
-    print(all(q_out_num_new == q_out_num))
+
     out_pro_bin_list = [
         q3d_num.GetYaxis().FindBin(y_dom[0]),
         q3d_num.GetYaxis().FindBin(y_dom[1]),
@@ -207,12 +260,11 @@ for analysis in femtolist:
     num_qout_a = root_numpy.hist2array(num_qout, include_overflow=True)
     print("==>")
     print("", q_out_num[:8])
-    print("", q_out_num_new[:8])
     print("", num_qout_a[:8])
-    diff = num_qout_a - q_out_num_new
+    diff = num_qout_a - q_out_num
 
     print("", diff[:10])
-    print("", num_qout_a == q_out_num_new)
+    print("", all(num_qout_a == q_out_num))
     break
 
 
@@ -242,110 +294,6 @@ for analysis in femtolist:
     ratio_ql = get_ratio('ratio_ql', num_qlong, den_qlong, [(-0.4, -0.7), (0.4, 0.7)])
     ratio_ql.SetTitle(make_cf_title(title='Q_{long}', units='q_{long}'))
     ratio_ql.Write()
-
-
-    #
-    # Do Fit
-    #
-
-    X = hist_method_to_array(ratio, 'GetBinCenter')
-    Y = hist_method_to_array(ratio, 'GetBinContent')
-    E = hist_method_to_array(ratio, 'GetBinError')
-
-    stop_fit = ratio.FindBin(0.25)
-
-    qinv_params = Parameters()
-    qinv_params.add('radius', value=0.5, min=0.0)
-    qinv_params.add('lam', value=0.5)  # , min=0.0, max=1.0)
-
-    def fit_qinv(params, x, data=None):
-        model = 1 + params['lam'] * np.exp(list(-(x * params['radius']) ** 2))
-        if data is None:
-            return model
-
-        shape = np.shape(data)
-        func_count = shape[0] / 2
-
-        if func_count % 1:
-            raise ValueError("Data shape not multiple of 2. Mismatched data and errors")
-
-        # resid = sum(np.sqrt((data[i] - model) ** 2 / data[i + 1] ** 2)
-        #             for i in range(0, shape[0], 2))
-
-        resid = model - data[0]
-        res = np.sqrt( resid ** 2 / data[1] ** 2 )
-        return res
-
-    x_vals = X[:stop_fit]
-    y_vals = Y[:stop_fit]
-    e_vals = E[:stop_fit]
-
-    qinv_fit = minimize(fit_qinv, qinv_params, args=(x_vals, (y_vals, e_vals)))
-    report_fit(qinv_fit)
-    FIT_X = np.linspace(300, (X[0], X[-1]))
-    FIT_Y = fit_qinv(qinv_fit.params, FIT_X)
-
-    canvas_qinv = gen_canvas()
-    # canvas_qinv.cd(1)
-
-    fit_plot = TGraph(len(FIT_X), FIT_X, FIT_Y)
-    CACHE.append(fit_plot)
-
-    ratio.Draw()
-    fit_plot.SetLineColor(2)
-    fit_plot.Draw("same")
-    canvas_qinv.Draw()
-
-
-
-    #
-    # Qinv - Kt binned
-    #
-    kt_qinv = get_root_object(analysis, ["KT_Qinv"])
-    if kt_qinv != None:
-        for ktbin in kt_qinv:
-            ktn = get_root_object(ktbin, ["Numc_qinv_pip", "Numc_qinv_pim"])
-            ktd = get_root_object(ktbin, ["Denc_qinv_pip", "Denc_qinv_pim"])
-
-
-
-    continue
-
-    #
-    #
-    #
-
-    def split_hist(hist):
-        return (
-            hist_method_to_array(hist, 'GetBinCenter'),
-            hist_method_to_array(hist, 'GetBinContent'),
-            hist_method_to_array(hist, 'GetBinError'),
-        )
-
-    Xo, Yo, Eo = split_hist(ratio_qo)
-    Xs, Ys, Es = split_hist(ratio_qs)
-    Xl, Yl, El = split_hist(ratio_ql)
-
-    qinv_params = Parameters()
-    qinv_params.add('r_out', value=0.5, min=0.0)
-    qinv_params.add('r_side', value=0.5, min=0.0)
-    qinv_params.add('r_long', value=0.5, min=0.0)
-    qinv_params.add('lam', value=0.5, min=-10.0, max=10.0)
-
-    def fit_3d_q(params, osl, data=None):
-        """
-        Do 3D Gaussian fit of data.
-
-        y = 1 + λ exp( -((qo * [Ro])^2 + (qs * [Rs])^2 + (ql * [Rl])^2) )
-
-        """
-        # split x into components
-        o, s, l = osl
-        t = sum((o * params['r_out']) ** 2, (s * params['r_side']) ** 2, (l * params['r_long']) ** 2)
-        model = 1 + params['lam'] * np.exp(list(-t))
-        if data is None:
-            return model
-
 
 
 input()
