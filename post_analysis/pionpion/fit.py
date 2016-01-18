467f61e6584efb682ab0cdb8365a09e94d8e67d0
Andrew Kubera <andrewkubera@gmail.com>
2016-01-18 13:54:00 -0500

    Added beginnings of a fit function which includes off-diagonal terms (R_out_side)

--- post_analysis/pionpion/fit.py
+++ post_analysis/pionpion/fit.py
@@ -144,3 +144,69 @@ def fitfunc_3d(params, q, data=None):
     res = np.sqrt((model - observed) ** 2 / err ** 2)
 
     return res
+
+
+
+def fitfunc_3d_with_offdiagonal_terms(params, q, data=None):
+    """
+    Do 3D Gaussian fit of data, including R_{os}, R_{ol}, R_{sl} terms in fit.
+
+    .. math::
+
+        CF(\\vec{q}) = NORM(1 + λ exp( -((qo * [Ro])^2 + (qs * [Rs])^2 + (ql * [Rl])^2
+            + 2 * qo * qs * [Ros]^2 + 2 * qo * ql * [Rol]^2+ 2 * qs * ql * [Rsl]^2)
+        ))
+
+    params:
+      norm - "global" normalization factor (unitless)
+      lam - lambda parameter (unitless)
+      r_out - radius of the out parameter (units: fm)
+      r_side - radius of the side parameter (units: fm)
+      r_long - radius of the long parameter (units: fm)
+
+      r_os - radius of the out parameter (units: fm)
+      r_ol - radius of the side parameter (units: fm)
+      r_sl - radius of the long parameter (units: fm)
+
+    q: 3xN matrix of each point in out-side-long space (units: GeV)
+    data: {optional} observed data to which we will calculate the residual
+    """
+
+    # extract parameters
+    norm = params['norm'].value
+    lam = params['lam'].value
+    r_out = params['r_out'].value
+    r_long = params['r_long'].value
+    r_side = params['r_side'].value
+
+    # create array of radii (1x3)
+    rr = np.array([r_out, r_side, r_long]) / HBAR_C
+
+    # create the exponent parameter by multiplying each radius by the
+    # appropriate q component (do shape check to ensure appropriate (1x3 * 3xN)
+    # matrix multiplication)
+    if np.shape(q)[1] == 3:
+        t = (rr * q) ** 2
+    else:
+        t = (rr * q.T) ** 2
+
+    # Sum along the q * r axis (t is now length N array)
+    t = np.sum(t, axis=1)
+    t += (2 * q[0] * q[1] * params['r_os'].value ** 2
+        + 2 * q[0] * q[2] * params['r_ol'].value ** 2
+        + 2 * q[1] * q[2] * params['r_sl'].value ** 2)
+
+    # Get the final (theoretical) result
+    model = norm * (1 + lam * np.exp(-t))
+
+    # we did not ask to compare to data - just return the model
+    if data is None:
+        return model
+
+    # split data and error
+    observed, err = data[:2]
+
+    # get residual result
+    res = np.sqrt((model - observed) ** 2 / err ** 2)
+
+    return res
