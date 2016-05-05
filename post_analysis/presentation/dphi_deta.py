f339573aceeef367d697072a0b3de4aec2e50165
Andrew Kubera <andrewkubera@gmail.com>
2016-05-05 09:44:36 -0400

    dφdη: Added code for output to file.

--- post_analysis/presentation/dphi_deta.py
+++ post_analysis/presentation/dphi_deta.py
@@ -7,8 +7,9 @@ Script which loops through femtolist and prints the configuration of each
 analysis.
 """
 import sys
+import lmfit
+from pionpion.femtolist import Femtolist
 from argparse import ArgumentParser
-from pionpion import Femtolist
 import ROOT
 
 parser = ArgumentParser()
@@ -17,28 +18,66 @@ parser.add_argument("--title-suffix",
                     nargs='?',
                     default='',
                     help="Adds the words to the end of the histogram title")
-parser.add_argument("root_filename", help="root filename to analyze")
-parser.add_argument("output_filename", help="root filename to analyze")
+parser.add_argument("root_filename",
+                    help="root filename to analyze")
+parser.add_argument("output_filename",
+                    default=None,
+                    help="root filename to analyze, use '-' to indicate no output")
 args = parser.parse_args()
 
 NUM_PATH = (
-    'NumDPhiStarDEta_dphi_deta',
+    'NumDPhiStarDEta_d',
+    'NumDPhiStarDEta_',
 )
 
 DEN_PATH = (
-    'DenDPhiStarDEta_dphi_deta',
+    'DenDPhiStarDEta_d',
+    'DenDPhiStarDEta_',
 )
+
+if args.output_filename != "-":
+    ofile = ROOT.TFile(args.output_filename, "RECREATE")
+else:
+    ofile = None
+
+def make_output_analysis(name):
+    if ofile is None:
+        from unittest.mock import Mock
+        return Mock()
+    return ofile.mkdir(name)
 
 for analysis in Femtolist(args.root_filename):
     num = analysis[NUM_PATH]
     den = analysis[DEN_PATH]
 
     if num == None or den == None:
-        print(analysis.GetName(), "Could not find numerator or denominator objects", file=sys.stderr)
+        print(analysis.name,
+              "Could not find numerator or denominator objects",
+              file=sys.stderr)
         continue
+
+
+
+    output = make_output_analysis(analysis.name)
 
     ratio = num.Clone("ratio")
     ratio.Divide(den)
     ratio.Draw("colz")
+    ratio.SetTitle("#Delta#eta vs #Delta#phi* - RATIO; #Delta#eta; #Delta#phi*")
+    ratio.SetStats(False)
 
-    ROOT.gApplication.Run()
+    output.WriteTObject(num, "numerator")
+    output.WriteTObject(den, "denominator")
+    output.WriteTObject(ratio)
+
+    x = ratio.ProjectionX()
+    x.SetTitle("#Delta#eta")
+    output.WriteTObject(x, "delta_eta")
+    x = ratio.ProjectionY()
+    x.SetTitle("#Delta#phi*")
+    output.WriteTObject(x, "delta_phi")
+
+    # input()
+
+if ofile:
+    ofile.Write()
