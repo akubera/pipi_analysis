MomentumCorrection: Fixed normalization of correction matrix.

post_analysis/momentum_correction.py
@@ -72,51 +72,50 @@ class Corrections:
         else:
             correction = Histogram.BuildFromRootHist(root_correction)
 
+        correction = correction.as_matrix()
+
         # normalize the correction matrix
         norm_correction = self.normalize_correction_hist_true(correction)
         # norm_correction = self.normalize_correction_hist_rec(correction)
         # norm_correction = self.normalize_correction_hist_both(correction)
 
-
         # cache and return result
         self._analyses[analysis_name] = norm_correction
         return norm_correction
 
-        norm = correction.data / norm_factor_0
-
     @staticmethod
-    def normalize_correction_hist_true(correction):
+    def normalize_correction_hist_true(correction_matrix):
         """
         Normalize a 2D Histogram (behaving as a matrix)
         Args:
             correction: Square 2D histogram with
         """
         # normalize so q_true (rows) sum to 1.0
-        norm_factor = correction.data.sum(axis=0, keepdims=True, dtype=float)
-        norm = correction.data / norm_factor
+        norm_factor = correction_matrix.sum(axis=1, keepdims=True, dtype=float)
+        norm = correction_matrix / norm_factor
         return norm
 
     @staticmethod
-    def normalize_correction_hist_rec(correction):
+    def normalize_correction_hist_rec(correction_matrix):
         """
         Normalize a 2D Histogram (behaving as a matrix)
         Args:
             correction: Square 2D histogram with
         """
         # normalize so q_rec (columns) sum to 1.0
-        norm_factor = correction.data.sum(axis=1, keepdims=True, dtype=float)
-        norm = correction.data / norm_factor
+        norm_factor = correction_matrix.sum(axis=0, keepdims=True, dtype=float)
+        norm = correction_matrix / norm_factor
         return norm
 
     @staticmethod
-    def normalize_correction_hist_both(correction):
+    def normalize_correction_hist_both(correction_matrix):
         # normalize along q_true
-        norm_factor_true = correction.data.sum(axis=0, keepdims=True, dtype=float)
+        norm_factor_true = correction_matrix.sum(axis=1, keepdims=True, dtype=float)
 
-        true_norm = correction.data / norm_factor_true
+        true_norm = correction_matrix / norm_factor_true
 
         # normalize along q_rec
-        norm_factor_reconstructed = true_norm.sum(axis=1, keepdims=True)
+        norm_factor_reconstructed = true_norm.sum(axis=0, keepdims=True)
 
         return true_norm / norm_factor_reconstructed
 
@@ -162,7 +161,9 @@ def main(argv):
         if norm_correction is None:
             print("No matching correction analysis %s. Skipping." % analysis.name)
             continue
-
+        print("\n** Normalized Correction Matrix **")
+        print(norm_correction)
+        print()
         # this applies the matrix to ALL qinv histograms
         analysis.apply_momentum_correction(norm_correction)
 

post_analysis/momentum_correction_sanity_test.py
@@ -85,6 +85,15 @@ correction_data = np.ascontiguousarray(np.array([
     [0.0, 0.0, 0.0, 2.0, 0.0],
     [0.0, 0.0, 0.0, 1.5, 1.5],
 ]).T)
+#
+# correction_data = np.ascontiguousarray(np.array([
+#     # q_rec ↦
+#     [0.8, 0.2, 0.0, 0.0, 0.0],  # q_true
+#     [1.0, 0.5, 0.3, 0.0, 0.0],  #  ↧
+#     [1.0, 0.0, 1.0, 0.0, 0.0],
+#     [1.0, 0.0, 0.0, 2.0, 0.0],
+#     [1.0, 0.0, 0.0, 1.5, 1.5],
+# ]).T)
 
 cfilename = "sanity_check/non_ident_correction.root"
 
@@ -135,20 +144,28 @@ momentum_correct([
 
 results = ROOT.TFile(output_filename, 'READ')
 cf_results = get_root_object(results, 'femtolist.analysis.Num_qinv_pip')
-# cf_expected = np.array([
-#     35.89 * 0.8 + 53.79 * 0.2,
-#     35.89 * 0.2 + 53.79 * 0.5 + 89.77 * 0.3,
-#     89.77,
-#     98.06,
-#     0.5 * (98.06 + 39.42)
-# ])
 cf_expected = np.array([
     35.89 * 0.8 + 53.79 * 0.2,
-    (35.89 * 0.2 + 53.79 * 0.5) / 0.7,
-    (89.77 * 1.0 + 53.79 * 0.3) / 1.3,
-    (98.06 * 4.0 + 39.42 * 3.0) / 7.0,
-    39.42
+    35.89 * 0.2 + 53.79 * 0.5 + 89.77 * 0.3,
+    89.77,
+    98.06,
+    98.06 * 0.5 + 39.42 * 0.5
 ])
+# cf_expected = np.array([
+#     35.89 * 0.8 + 53.79 * 0.28571429,
+#     35.89 * 0.2 + 53.79 * 0.71428571 + 89.77 * 0.23076923,
+#     89.77 * 0.76923077,
+#     98.06 * 0.57142857,
+#     98.06 * 0.42857143 + 39.42 * 1.0
+# ])
+# cf_expected = np.array([
+#     35.89 * 0.8 + 53.79 * 0.28571429,
+#     35.89 * 0.2 + 53.79 * 0.71428571 + 89.77 * 0.23076923,
+#     89.77 * 0.76923077,
+#     98.06 * 0.66666667,
+#     98.06 * 0.33333333 + 39.42 * 1.0
+# ])
+
 cf_res_array = hist2array(cf_results)
 matches = cf_res_array == cf_expected
 print(matches)