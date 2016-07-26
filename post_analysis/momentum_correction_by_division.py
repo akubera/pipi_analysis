42219ed2bc15dc052e4fa208db662102b5e35c1d
Andrew Kubera <andrewkubera@gmail.com>
2016-07-26 12:20:56 -0400

    momentum_correction_by_division: Added kT bin output.

--- post_analysis/momentum_correction_by_division.py
+++ post_analysis/momentum_correction_by_division.py
@@ -13,6 +13,7 @@ from stumpy.utils import root_histogram_datatype
 from argparse import ArgumentParser
 from unittest.mock import MagicMock
 from pionpion.femtolist import Femtolist
+from pionpion.root_helpers import get_root_object
 import ROOT
 
 TRUE_NUM_PATH = ['CF_Num']
@@ -38,7 +39,11 @@ def parser():
 
 class Corrections:
     """
-    A class wrapping the retrieval of MonteCarlo 'true' correlation functions.
+    A class wrapping the retrieval of MonteCarlo 'true' correlation
+    functions. This is given a root file, and all numerator/denominator
+    histogram pairs of montecarlo data are found and the corrections
+    calculated.
+
     """
 
     def __init__(self, filename):
@@ -63,48 +68,58 @@ class Corrections:
         try:
             analysis = self._femtolist[analysis_name]
         except KeyError:
-            print("XXX")
             return None
         else:
-            print("YYY")
             if analysis is None:
-                print("ZZZ")
                 return None
 
-        # load 'true' correlation functions from analysis
-        true_num = analysis[TRUE_NUM_PATH]
-        true_den = analysis[TRUE_DEN_PATH]
-        print(true_num, true_den)
+        def get_data_from_hist(root_hist):
+            nbins = root_hist.GetNbinsX()
+            dtype = root_histogram_datatype(root_hist)
+            data = np.frombuffer(root_hist.GetArray(), dtype=dtype, count=nbins + 2)
+            # ignore overflow+underflow bins
+            return np.copy(data[1:-1])
 
-        rec_num = analysis[analysis.QINV_NUM_PATH]
-        rec_den = analysis[analysis.QINV_DEN_PATH]
-        print(rec_num, rec_den)
+        data = get_data_from_hist(self.make_ratio(*self.get_cf_hists(analysis._data)))
 
+        result = self.Result()
+        result.base = data
+        result.kt_bins = {}
+
+        if analysis.has_kt_bins():
+            print("Momentum Correction analysis has kT bins")
+
+            for b in analysis.kt_binned_pairs:
+                bin_name = b.GetName()
+                ratio = get_data_from_hist(self.make_ratio(*self.get_cf_hists(b)))
+                result.kt_bins[bin_name] = ratio
+
+        self._analyses[analysis_name] = result
+        return result
+
+    class Result:
+        pass
+
+    @staticmethod
+    def get_cf_hists(obj):
+        return (
+            get_root_object(obj, 'NumTrueIdeal_MC_CF'),
+            get_root_object(obj, 'DenIdeal_MC_CF'),
+            get_root_object(obj, 'NumTrue_MC_CF'),
+            get_root_object(obj, 'Den_MC_CF')
+        )
+
+    @staticmethod
+    def make_ratio(true_num, true_den, fake_num, fake_den):
         true_ratio = true_num.Clone()
-        rec_ratio = rec_num.Clone()
-
         true_ratio.Divide(true_den)
-        rec_ratio.Divide(rec_den)
+
+        rec_ratio = fake_num.Clone()
+        rec_ratio.Divide(fake_den)
 
         double_ratio = true_ratio.Clone()
         double_ratio.Divide(rec_ratio)
-
-        # true_ratio.Draw()
-        # input()
-        # rec_ratio.Draw()
-        # input()
-        # double_ratio.Draw()
-        # input()
-
-        nbins = double_ratio.GetNbinsX()
-        dtype = root_histogram_datatype(double_ratio)
-        data = np.frombuffer(double_ratio.GetArray(), dtype=dtype, count=nbins + 2)
-
-        # ignore overflow+underflow bins
-        data = np.copy(data[1:-1])
-        self._analyses[analysis_name] = data
-
-        return data
+        return double_ratio
 
     @property
     def file(self):
@@ -119,6 +134,56 @@ class Corrections:
         self._femtolist = Femtolist(self._file)
         self._analyses = {}
         print("# Opened correction file:", value)
+
+def take_hist_ratio(num, den, name="ratio", title="Ratio"):
+    ratio = num.Clone(name)
+    ratio.Divide(den)
+    ratio.SetTitle(title)
+    return ratio
+
+def apply_correction_vector(root_hist_num, root_hist_den, norm_correction):
+    num, den = root_hist_num, root_hist_den
+    norm_dim_matches = num.GetNbinsX() == norm_correction.shape[0]
+    num_den_dim_match = num.GetNbinsX() == den.GetNbinsX()
+
+    if not num_den_dim_match:
+        raise TypeError("Error, Numerator and Denominator do NOT match dimensions. Skipping.")
+
+    elif not norm_dim_matches:
+        a = norm_correction.shape[0]
+        b = num.GetNbinsX()
+
+        print("Error, correction histogram has different shape than data. "
+              "{} ≠ {}. Attempting rebin...".format(a, b),
+              file=sys.stderr, end='')
+
+        try:
+            from math import gcd
+        except ImportError: # python < 3.5 shim
+            from fractions import gcd
+        g = gcd(a, b)
+        lcm = a * b / gcd(a, b)
+        if not lcm or lcm % 1 != 0.0:
+            raise ValueError("LCM could not be found. Skipping.")
+
+        if a < b:
+            scale = int(lcm // a)
+            print("Rebinning by %s." % scale, file=sys.stderr)
+            num.Rebin(scale)
+            den.Rebin(scale)
+        else:
+            print("UNIMPLEMENTED... by %0.1f." % (lcm / a), file=sys.stderr)
+            num.Rebin(a / lcm)
+            den.Rebin(a / lcm)
+            # norm_correction = norm_correction[::2] + norm_correction[1::2]
+
+    root_ratio = take_hist_ratio(num, den, "cCF", "Corrected Correlation Function")
+    for i, c in enumerate(norm_correction, 1):
+        x = root_ratio.GetBinContent(i)
+        e = root_ratio.GetBinError(i)
+        root_ratio.SetBinContent(i, x * c)
+        root_ratio.SetBinError(i, e * c)
+    return root_ratio
 
 
 def main(argv):
@@ -146,48 +211,50 @@ def main(argv):
 
     # loop through analyses
     for analysis in femtolist:
-        try:
-            norm_correction = corrections[analysis.name]
-        except IndexError:
+
+        norm_correction = corrections[analysis.name]
+
+        if norm_correction is None:
             print("No matching correction analysis %s. Skipping." % analysis.name)
             continue
+
         print("\n** Normalized Correction Data **")
-        print(norm_correction)
+        print(norm_correction.base)
         print()
 
-        # num, den = analysis.qinv_pair
         num, den = analysis[analysis.QINV_NUM_PATH], analysis[analysis.QINV_DEN_PATH]
 
-        # if num.GetXaxis().GetXmax() != norm_correction.
-        if num.GetNbinsX() != norm_correction.shape[0]:
-            factor = num.GetNbinsX() // norm_correction.shape[0]
-            num = num.Rebin(factor)
+        analysis_output = output_femtolist.mkdir(analysis.name)
+        analysis_output.cd()
 
-        if den.GetNbinsX() != norm_correction.shape[0]:
-            factor = den.GetNbinsX() // norm_correction.shape[0]
-            den = den.Rebin(factor)
-            # if (num.GetNbinsX() / norm_correction.shape) % 1 == 0.0:
+        try:
+            root_ratio = apply_correction_vector(num, den, norm_correction.base)
+        except Exception as err:
+            print(err, file=sys.stderr)
+            output_femtolist.rmdir(analysis.name)
+            continue
 
+        root_ratio.Write()
 
-        num, den = Histogram.BuildFromRootHist(num), Histogram.BuildFromRootHist(den)
-        ratio = num / den
-        print('ratio', num.shape, den.shape, norm_correction.shape)
-        corrected_ratio = ratio * norm_correction
+        for kt_data in analysis.kt_binned_pairs:
+            bin_name = kt_data.GetName()
+            # ROOT.TDirectory.Cd(analysis.name)
+            path = "%s/%s/" % (analysis.KT_BINNED_ANALYSIS_PATH[0], bin_name)
+            analysis_output.mkdir(path).cd()
+            # analysis_output.mkdir()
+            try:
+                correction = norm_correction.kt_bins[bin_name]
+            except KeyError:
+                print("No kt_bin '%s' in correction file" % bin_name, file=sys.stderr)
+                continue
 
-        print("\n** Corrected CF **")
-        print(ratio)
-        print()
-
-        # import matplotlib.pyplot as plt
-        # plt.plot(ratio.data[:30])
-        # plt.plot(corrected_ratio.data[:30])
-        # plt.show()
-
-        # this applies the matrix to ALL qinv histograms
-        # analysis.apply_momentum_correction(norm_correction)
-
-        # write the smeared output
-        analysis.write_into(output_femtolist)
+            num = get_root_object(kt_data, analysis.QINV_NUM_PATH)
+            den = get_root_object(kt_data, analysis.QINV_DEN_PATH)
+            analysis_output.cd(path)
+            cCF = apply_correction_vector(num, den, correction)
+            cf = take_hist_ratio(num, den, "CF", "(UnCorrected) Correlation Function")
+            cf.Write()
+            cCF.Write()
 
     output.Close()
 
