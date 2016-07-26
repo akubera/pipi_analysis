1b7721ef3b6370f1c2082930b03185b4bc6cff54
Andrew Kubera <andrewkubera@gmail.com>
2016-07-26 12:28:52 -0400

    qinv_fit: Added correction output to fit

--- post_analysis/qinv_fit.py
+++ post_analysis/qinv_fit.py
@@ -21,6 +21,7 @@ from lmfit import minimize, Parameters, report_fit
 from rootpy.plotting import HistStack
 from rootpy.plotting.utils import draw
 
+from stumpy import Histogram
 from pionpion import Femtolist
 from pionpion.fit import fitfunc_qinv, fitfunc_qinv_gauss_ll
 from post_analysis.fitting.gaussian import (
@@ -28,6 +29,7 @@ from post_analysis.fitting.gaussian import (
     GaussianModelCoulomb,
     GaussianModelFSI,
 )
+from momentum_correction_by_division import Corrections
 
 import ROOT  # import root last
 
@@ -47,6 +49,10 @@ def argument_parser():
                         nargs='?',
                         default='0.0:0.5',
                         help="Range of normalization")
+    parser.add_argument("--correction-file",
+                        nargs='?',
+                        default=None,
+                        help="File containing momentum correction histograms.")
     parser.add_argument("--title-suffix",
                         type=str,
                         nargs='?',
@@ -68,6 +74,15 @@ def argument_parser():
                              "inserted into the input name. A value of '-' indicates no "
                              "output file")
     return parser
+
+
+def simple_fit(ratio, ModelClass=GaussianModelFSI, fit_range=(1, 0.16)):
+    qinv_params = ModelClass.guess()
+    fit_slice = ratio.x_axis.get_slice(fit_range)
+    x = ratio.x_axis.bin_centers[fit_slice]
+    return minimize(ModelClass.as_resid,
+                    qinv_params,
+                    args=(x, ratio[fit_slice], ratio.errors[fit_slice]))
 
 
 def loglikely_fit(num, den, ModelClass=GaussianModelFSI, fit_range=(0, 0.16)):
@@ -94,14 +109,12 @@ def loglikely_fit(num, den, ModelClass=GaussianModelFSI, fit_range=(0, 0.16)):
     return qinv_fit
 
 
-def get_fitting_procedure():
+def get_fitting_procedure(args):
     """
-    ** currently unused **
-
     Function (which will be) used to select a fitting method based on
     program arguments.
     """
-    return loglikely_fit
+    return loglikely_fit if args.use_ll else simple_fit
 
 
 def do_timed_fit(fitter):
@@ -141,8 +154,11 @@ class QinvFit:
         Construct a fit from an analysis (located in a femtolist)
         """
         print("***", analysis.name)
-        self.fit_res = fit_res = self.do_fit(analysis.qinv_pair)
         self.name = analysis.name
+        self.analysis = analysis
+        self.fit_procedure = get_fitting_procedure(args)
+
+        # self.fit_res = self.do_fit(analysis.qinv_pair)
 
         if not args.do_ktbin:
             self.kt_binned_fits = None
@@ -167,8 +183,23 @@ class QinvFit:
                     'lambda_err': lam.stderr,
                 }
 
+    @property
+    def corrections(self):
+        "Return momentum correction histogram for the qinv fit (if available)"
+        global corrections
+        try:
+            return corrections[self.name]
+        except:
+            return None
+
     def do_fit(self, qinv_pair):
-        return loglikely_fit(*qinv_pair)
+        return self.fit_procedure(*qinv_pair)
+
+    @staticmethod
+    def fit_result_to_plot(fit_res):
+        rplot = ROOT.TGraphErrors(npoints, x, r, np.zeros(npoints), re)
+        rplot.SetName("CF")
+        output.Add(rplot)
 
     def write(self, file):
         """
@@ -177,9 +208,21 @@ class QinvFit:
         """
 
         if isinstance(file, ROOT.TDirectory):
+            print("into", file)
             file.cd()
             output = ROOT.TObjArray()
             output.SetName(self.name)
+            num, den = self.analysis.qinv_pair
+            ratio = num / den
+            print('...', num.data[-1], den.data[-1])
+            norm = self.fit_res.params['norm'].value
+            print('norm:', norm)
+            print(ratio.data[-1])
+            ratio /= norm
+            print(ratio.data[-1])
+            output.Add(ratio.AsRootHist())
+            # output.Add(self.fit_result_to_plot(self.fit_res))
+
             if self.kt_binned_fits:
                 f = self.kt_binned_fits.values()
 
@@ -202,39 +245,115 @@ class QinvFit:
 
 def main(argv):
     """ Main Function of qinv_fit """
+    global corrections
+
     # parse user arguments
     args = argument_parser().parse_args(argv)
 
+    # assign global corrections built from the correction file
+    if args.correction_file:
+        corrections = Corrections(args.correction_file)
+    else:
+        corrections = None
+
+    # get normalization and output domains from arguments
     norm_x_range = tuple(map(float, args.norm_range.split(':')))
     output_domain = tuple(map(float, args.output_domain.split(':')))
 
+    # get filenames separated by commas
     femtolist_names = tuple(map(str.strip, args.datafile.split(',')))
 
     # create the femtolist(s) from the input file(s)
     femtolists = tuple(map(Femtolist, femtolist_names))
 
-    # generate filename from input name
+    # generate filename from (first) input name
     if args.output_filename is None:
-        filename = femtolist_names[0]
-        ext_position = filename.rfind('.')
-        fname, ext = filename[:ext_position], filename[ext_position:]
-        args.output_filename =  "%s.qinv%s" % (fname, ext)
+        fname, _, ext = femtolist_names[0].rpartition('.')
+        args.output_filename =  "%s.qinv.%s" % (fname, ext)
 
     # create outputfile (Mock object if output_filename is '-')
-    output_file = (ROOT.TFile(args.output_filename, 'RECREATE')
-                   if args.output_filename != '-'
-                   else MagicMock())
+    if args.output_filename == '-':
+        output_file = MagicMock()
+    else:
+        output_file = ROOT.TFile(args.output_filename, 'RECREATE')
 
-    # Create fit object from each analysis in femtolist
+    # loop through each file
     for femtolist in femtolists:
-        fname = basename(femtolist._file.GetName().rstrip(".root"))
-        d = output_file.mkdir(fname)
-        d.cd()
+        # if multiple inputs, store in multiple 'subdirs'
+        if len(femtolists) == 1:
+            d = output_file
+        else:
+            d = output_file.mkdir(femtolist.filename.rstrip(".root"))
+
+        # Create fit object from each analysis in femtolist
         for analysis in femtolist:
-            qfit = QinvFit(analysis, args)
-            o = d.mkdir(analysis.name)
-            if o != None:
-                qfit.write(o)
+
+            d.mkdir(analysis.name).cd()
+
+            num, den = analysis.qinv_pair
+            ratio = num / den
+            ratio.title = "Corrected Correlation Function"
+            print(ratio.errors[:5])
+            if corrections:
+                norm_correction = corrections[analysis.name]
+                assert ratio.shape == norm_correction.shape, \
+                       "{} ≠ {}".format(ratio.shape, norm_correction.shape)
+                ratio *= norm_correction
+            print(ratio.errors[:5])
+
+            fit_res = simple_fit(ratio)
+            report_fit(fit_res)
+
+            # import matplotlib.pyplot as plt
+            # plt.errorbar(x=list(range(30)), y=ratio.data[:30], yerr=ratio.errors[:30])
+            # plt.plot(GaussianModelFSI.gauss(
+            #     ratio.x_axis.bin_centers[:30],
+            #     **fit_res.params))
+            # plt.show()
+
+
+            num, den = analysis[analysis.QINV_NUM_PATH], analysis[analysis.QINV_DEN_PATH]
+
+            # create ratio
+            root_ratio = num.Clone()
+            root_ratio.SetTitle("Corrected Correlation Function")
+            root_ratio.Divide(den)
+
+            # apply correction
+            if corrections:
+                norm_correction = corrections[analysis.name]
+                assert num.GetNbinsX() == den.GetNbinsX() == norm_correction.shape[0]
+                for i, c in enumerate(norm_correction, 1):
+                    x = root_ratio.GetBinContent(i)
+                    e = root_ratio.GetBinError(i)
+                    root_ratio.SetBinContent(i, x * c)
+                    root_ratio.SetBinError(i, e * c)
+                root_ratio.Write("CorrelationFunction")
+
+            #
+            fit_res = simple_fit(Histogram.BuildFromRootHist(root_ratio))
+            report_fit(fit_res)
+
+            x = np.linspace(0, 1.0, 300)
+            y = GaussianModelFSI.gauss(x, **fit_res.params)
+            plot = ROOT.TGraph(len(x), x, y)
+            plot.Write("FitPlot")
+            # x = np.copy(ratio.x_axis.bin_centers[:30])
+            # y = np.copy(GaussianModelFSI.gauss(x, **fit_res.params))
+            # ye = np.copy(np.sqrt(np.mean(data[2] ** 2, axis=1)))
+            #
+            # fit_plot = ROOT.TGraphErrors(len(x), x, y, np.zeros(len(x)), ye)
+            # plt.plot(
+            #     ratio.x_axis.bin_centers[:30],
+            #     **fit_res.params))
+            # qfit = QinvFit(analysis, args)
+            # report_fit(qfit.fit_res)
+
+            # o = d.mkdir(analysis.name)
+            # print('.>.', o)
+            # if o != None:
+            #     qfit.write(o)
+            #     print('wrote', qfit)
 
     output_file.Write()
     output_file.Close()
