baa4e19920b35117fac71e0a73677dae424d26af
Andrew Kubera <andrewkubera@gmail.com>
2016-08-01 16:47:39 +0200

    ktbinned_qinv: Removed dead code and the log-likelihood fitting procedure

--- post_analysis/ktbinned_qinv_fit.py
+++ post_analysis/ktbinned_qinv_fit.py
@@ -7,6 +7,7 @@ import sys
 import time
 import pionpion
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from pprint import pprint
@@ -49,124 +50,95 @@ def argument_parser():
     return parser
 
 
-def do_qinv_fit(num, den, ModelClass, fit_range, use_loglikely=True):
+def do_qinv_fit(ratio, ModelClass, fit_range):
     """
     Do a fit of numerator denominator pairs.
     """
-    ratio = num / den
+    # ratio = num / den
     qinv_params = ModelClass.guess(ratio)
-    fit_slice = ratio.x_axis.get_slice(FIT_RANGE)
-    x = ratio.x_axis.bin_centers[fit_slice]
+    fit_slice = ratio.x_axis.get_slice(fit_range)
 
-    if use_loglikely:
-        qinv_fit = minimize(ModelClass.as_loglike, qinv_params, args=(x, num[fit_slice], den[fit_slice]))
-    else:
-        y = ratio.data[fit_slice]
-        e = ratio.errors[fit_slice]
-        qinv_fit = minimize(ModelClass.as_resid, qinv_params, args=(x, y, e))
+    y = ratio.data[fit_slice]
+    nonzero_mask = y != 0
+
+    x = ratio.x_axis.bin_centers[fit_slice][nonzero_mask]
+    y = y[nonzero_mask]
+    e = ratio.errors[fit_slice][nonzero_mask]
+    # print('y', y)
+    # print('e', e)
+    qinv_fit = minimize(ModelClass.as_resid, qinv_params, args=(x, y, e))
 
     return qinv_fit
+
+
+def plot_cf_and_fit(cf, fit_res, ModelClass=GaussianModelFSI):
+    """
+    Plot a correlation with line of best fit as determined by some
+    fit results.
+    """
+    if isinstance(cf, Histogram):
+        cf = cf.AsRootHist()
+
+    FIT_X = np.linspace(0.0, 0.16, 300)
+    FIT_Y = ModelClass().eval(fit_res.params, x=FIT_X)
+    cf.GetXaxis().SetRangeUser(0, 0.3)
+    cf.Draw()
+    fit_plot = ROOT.TGraph(len(FIT_X), FIT_X, FIT_Y)
+    fit_plot.SetLineColor(2)
+    fit_plot.Draw("Same")
+
+    input()
 
 
 def main(argv):
 
     args = argument_parser().parse_args(argv)
 
+    if args.use_ll:
+        print("LogLikelihood not implemented for kT-binned fit! Aborting.", file=sys.stderr)
+        return
+
     # create the femtolist from the input file
     femtolist = Femtolist(args.datafile)
 
     # no exception means femtolist is good - create outputfile
     if args.output_filename is None:
-        ext_position = args.datafile.rfind('.')
-        args.output_filename = args.datafile[:ext_position] + ".kt_qinv" + args.datafile[ext_position:]
+        basename, _, ext = args.datafile.rpartition('.')
+        args.output_filename = basename + ".kt_qinv." + ext
 
     output_file = ROOT.TFile(args.output_filename, 'RECREATE')
     fit_range = args.fit_range.split(':')
     FIT_CLASS = GaussianModelFSI
 
     for analysis in femtolist:
-        print("***", analysis.name)
+        print("\n***", analysis.name)
         kt_analysis = analysis['KT_Qinv']
 
-        if not kt_analysis:
-            print("No Kt Analysis found")
-            continue
+        root_cf = analysis['CF']
+        cf = Histogram.BuildFromRootHist(root_cf)
+        cf_fit_res = do_qinv_fit(cf, FIT_CLASS, (0, 0.16))
 
-        # for kt_bin in kt_analysis:
-        for kt_bin in analysis.kt_binned_pairs:
-            print(kt_bin)
-            name = kt_bin.GetName()
-            num = Histogram.BuildFromRootHist(get_root_object(kt_bin, Analysis.QINV_NUM_PATH))
-            den = Histogram.BuildFromRootHist(get_root_object(kt_bin, Analysis.QINV_DEN_PATH))
+        report_fit(cf_fit_res)
 
-            fit_res = do_qinv_fit(num, den, FIT_CLASS)
-            report_fit(fit_res, show_correl=False)
+        plot_cf_and_fit(root_cf, cf_fit_res)
 
-            x = np.linspace(0, 1.0, 300)
-            y = FIT_CLASS.gauss(x, **fit_res.params)
-            plot = ROOT.TGraph(len(x), x, y)
-            plot.Write()
-            # plt.plot(x, y)
-            # plt.show()
-            input()
-            continue
+        for x in list(map(ROOT.TKey.ReadObj, kt_analysis.GetListOfKeys())):
+            root_cf = get_root_object(x, 'CF')
+            root_ccf = get_root_object(x, 'cCF')
 
-            print("\n:::Chi-Squared Fit:::::")
-            # norm_ratio = num / np.sum(num[0.7:0.8]) / (den / np.sum(den[0.7:0.8]))
-            norm_ratio = num / den
+            cf = Histogram.BuildFromRootHist(root_cf)
+            corrected_cf = Histogram.BuildFromRootHist(root_ccf)
 
+            cf_fit_res = do_qinv_fit(cf, FIT_CLASS, (0, 0.16))
+            corrected_fit_res = do_qinv_fit(corrected_cf, FIT_CLASS, (0, 0.16))
 
-            qinv_params = Parameters()
-            qinv_params.add('radius', value=6.5, min=0.0)
-            qinv_params.add('lam', value=0.5)  # , min=0.0, max=1.0)
-            qinv_params.add('norm', value=1.0)  # , min=0.0, max=1.0)
-
-            fit_method = 'leastsq'
-
-            b = norm_ratio.bin_at(0.2)
-            # b = 28
-            print('b:',b)
-            fit_args = num.x_axis.bin_centers[:b], (norm_ratio.data[:b], norm_ratio.errors[:b])
-
-            tfit = minimize(fitfunc_qinv, qinv_params, args=fit_args, method=fit_method)
-
-            fitx = np.linspace(0, 0.1, 100)
-            fity = fitfunc_qinv(tfit.params, fitx)
-            report_fit(tfit)
-            print("\n:::LL-Fit:::::")
-
-            qinv_params = Parameters()
-            # qinv_params.add('radius', value=tfit.params['radius'].value, min=0.0)
-            # qinv_params.add('lam', value=tfit.params['lam'].value)
-            qinv_params.add('radius', value=6.5, min=0.0)
-            qinv_params.add('lam', value=0.5)
-            qinv_params.add('norm', value=1)
-
-
-
-            fit_args = (num.x_axis.bin_centers[:b], (num.data[:b], den.data[:b]))
-
-            # print("    ::"),
-            # pprint(list(zip(fit_args[0], *fit_args[1])))
-
-            TIMESTART = time.monotonic()
-            qinv_fit = minimize(fitfunc_qinv_gauss_ll, qinv_params, args=fit_args, method=fit_method)
-            TIME_DELTA = time.monotonic() - TIMESTART
-            time_per_call = TIME_DELTA * 1e3 / qinv_fit.nfev
-            report_fit(qinv_fit)
-            print("fitting time %0.3fs (%0.3f ms/call)" % (TIME_DELTA, time_per_call))
-
-            fitll = fitfunc_qinv(qinv_fit.params, fitx)
-            plt.plot(fitx, fity, 'r-',
-                     fitx, fitll, 'g-',)
-
-            plt.errorbar(norm_ratio.x_axis.bin_centers[:b],
-                         norm_ratio.data[:b],
-                         yerr=norm_ratio.errors[:b],
-                         fmt='b.',
-                         )
-            plt.title = name
-            plt.show()
+            # pd.DataFrame()
+            # print(res.valuesdict())
+            print()
+            print()
+            report_fit(corrected_fit_res)
+            plot_cf_and_fit(root_ccf, corrected_fit_res)
+            # print(dict(**cf_fit_res.params))
 
     output_file.Close()
 
