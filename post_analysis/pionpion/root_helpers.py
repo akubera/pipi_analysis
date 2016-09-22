e4d2b3c22a38b9622dad4501fce4799b420c016b
Andrew Kubera <andrew.michael.kubera@cern.ch>
2016-09-22 02:25:00 -0400

    root_helpers: Reimplemented get_root_object to be more correct

--- post_analysis/pionpion/root_helpers.py
+++ post_analysis/pionpion/root_helpers.py
@@ -10,22 +10,20 @@ def get_root_object(obj, paths):
     Return a root object contained in the obj
     """
     if isinstance(paths, (list, tuple)):
-        if len(paths):
-            path, paths = paths[0], paths[1:]
-        else:
-            return None
-    else:
-        path, paths = paths, []
+        for path in paths:
+            found = get_root_object(obj, path)
+            if found != None:
+                return found
+        return None
 
-    key, *rest = path.split('.', 1)
+    key, *rest = paths.split('.', 1)
+
     try:
         new_obj = obj.Get(key)
     except AttributeError:
         new_obj = obj.FindObject(key)
 
-    if new_obj == None and len(paths) is not 0:
-        return get_root_object(obj, paths)
-    elif new_obj == None or len(rest) is 0:
-        return new_obj
-    else:
+    if rest:
         return get_root_object(new_obj, rest[0])
+    else:
+        return new_obj
