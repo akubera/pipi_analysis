2fdad93578c055e29b60115725236648a9fd0704
Andrew Kubera <andrew.michael.kubera@cern.ch>
2016-12-15 09:03:59 -0500

    q3d: Using stumpy histograms instead of ROOT histograms for fitting & slicing

--- post_analysis/pionpion/q3d.py
+++ post_analysis/pionpion/q3d.py
@@ -9,6 +9,7 @@ histograms containing Q_out Q_side Q_long particle data
 import numpy as np
 import numpy.ma as ma
 from itertools import starmap
+from functools import partialmethod
 from stumpy import Histogram
 
 
@@ -19,28 +20,38 @@ class Q3D:
     """
 
     def __init__(self, numerator, denominator, do_sanity_check=True):
-        self.num = Histogram.BuildFromRootHist(numerator)
-        self.den = Histogram.BuildFromRootHist(denominator)
+        from ROOT import TH1
+        num_root, den_root = None, None
+        if isinstance(numerator, TH1):
+            num_root = numerator
+            numerator = Histogram.BuildFromRootHist(numerator)
+        if isinstance(denominator, TH1):
+            den_root = denominator
+            denominator = Histogram.BuildFromRootHist(denominator)
 
-        ratio = numerator.Clone("ratio")
-        ratio.Divide(denominator)
-        self.ratio = Histogram.BuildFromRootHist(ratio)
-        # self.ratio.errors = np.sqrt(self.ratio.data)
+        self.num = numerator
+        self.den = denominator
 
         assert self.num.shape == self.den.shape
         assert all(n[i] == d[i]
                    for n, d in zip(self.num.axes, self.den.axes)
                    for i in (2, 10, -2))
 
+        self.ratio = numerator / denominator
+        # ratio = numerator.Clone("ratio")
+        # ratio.Divide(denominator)
+        # self.ratio = Histogram.BuildFromRootHist(ratio)
+        # self.ratio.errors = np.sqrt(self.ratio.data)
+
 
         if do_sanity_check:
             sanity_bins = self.num.get_slice((-.1, .1), (-.1, .1), (-.1, .1))
-            sanity_data = np.array([
+            sanity_data = np.fromiter((
                 self.num[x, y, z]
                 for x in range(sanity_bins[0].start, sanity_bins[0].stop)
                 for y in range(sanity_bins[1].start, sanity_bins[1].stop)
                 for z in range(sanity_bins[2].start, sanity_bins[2].stop)
-            ])
+            ), np.float64)
             # simple sanity test
             assert all(self.num[sanity_bins].flatten() == sanity_data)
 
@@ -55,9 +66,9 @@ class Q3D:
         #
         # self.ratio._ptr = numerator.Clone("_ptr")
         # self.ratio._ptr.Divide(numerator, denominator)
-        self.num._ptr = numerator
-        self.den._ptr = denominator
-        self.ratio._ptr = ratio
+        self.num._ptr = num_root
+        self.den._ptr = den_root
+        # self.ratio._ptr = num_root
         # print(self.ratio.data - self.ratio_data)
         assert (self.ratio.data == self.ratio_data).all(), "Error %f" % (np.max(self.ratio.data - self.ratio_data))
 
@@ -93,42 +104,89 @@ class Q3D:
         slices = tuple(starmap(dom_to_slice, zip(self.num.axes, domain_tuples)))
         return slices
 
-    def projection_out(self, y_domain=(-0.1, 0.1), z_domain=(-0.1, 0.1)):
-        """
-        Returns a numpy array of the entire 'out' axis, with the other two axes
-        restricted by the parameters
-        """
-        # change domains into array slices
-        # x_slice, y_slice, z_slice = self.bins_to_slices(
-        #     y_domain=y_domain,
-        #     z_domain=z_domain
-        # )
-        # x_slice = self.bins_to_slices
-        #
-        # num_slice = self.num[x_slice, y_slice, z_slice]
-        # den_slice = self.den[x_slice, y_slice, z_slice]
-        # self.get_projection()
-        # data = num_slice / den_slice
-        # # print("Sliced data", data.shape)
-        # # print(data)
-        # sum = data.sum(axis=(1, 2))
+    # def projection_out(self, y_domain=(-0.1, 0.1), z_domain=(-0.1, 0.1)):
+    #     """
+    #     Returns a numpy array of the entire 'out' axis, with the other two axes
+    #     restricted by the parameters
+    #     """
+    #     # change domains into array slices
+    #     # x_slice, y_slice, z_slice = self.bins_to_slices(
+    #     #     y_domain=y_domain,
+    #     #     z_domain=z_domain
+    #     # )
+    #     # x_slice = self.bins_to_slices
+    #     #
+    #     # num_slice = self.num[x_slice, y_slice, z_slice]
+    #     # den_slice = self.den[x_slice, y_slice, z_slice]
+    #     # self.get_projection()
+    #     # data = num_slice / den_slice
+    #     # # print("Sliced data", data.shape)
+    #     # # print(data)
+    #     # sum = data.sum(axis=(1, 2))
+    #     s = self.ratio.get_slice(None, y_domain, z_domain)
+    #     data = self.ratio[:, y_domain, z_domain]
+    #     sum = data.sum(axis=(1, 2)) / (s[1].stop - s[1].start) / (s[2].stop - s[2].start)
+    #     return sum
 
-        data = self.ratio[:,y_domain,z_domain]
-        sum = data.sum(axis=(1, 2))
+    def project(self, axis, x_slice, y_slice, z_slice):
+        reduce_axes = {0: (1, 2), 1: (0, 2), 2: (0, 1)}[axis]
+        data = self.ratio[x_slice, y_slice, z_slice]
+        sum = data.sum(axis=reduce_axes)
+        # sum = data.sum(axis=(1, 2)) / (s[1].stop - s[1].start) / (s[2].stop - s[2].start)
+        return sum[0]
+
+    # projection_out = partialmethod(lambda self, y_domain, z_domain: self.project(0, None, y_domain, z_domain))
+    projection_out = partialmethod(project, axis=0, x_slice=None)
+    projection_side = partialmethod(project, axis=1, y_slice=None)
+    projection_long = partialmethod(project, axis=2, z_slice=None)
+
+    def projection_error(self, axis, x_slice, y_slice, z_slice):
+        """
+        Project error along a 1D axis.
+        """
+        reduce_axes = {0: (1, 2), 1: (0, 2), 2: (0, 1)}[axis]
+        s = self.ratio.get_slice(x_slice, y_slice, z_slice)
+        data = self.ratio.errors[s]
+        sum = np.sqrt((data ** 2).sum(axis=reduce_axes))[0]
         return sum
+        # along_axis = 3 - (axes[0] + axes[1])
+        # np.swapaxes(axes, 0, along_axis)
 
-    def projection_side(self, x_domain=(-0.1, 0.1), z_domain=(-0.1, 0.1)):
-        """
-        Returns a numpy array of the entire 'side' axis, with the other two
-        axes restricted by the parameters
-        """
-        data = self.ratio[x_domain,:,z_domain]
+
+    projection_out_error = partialmethod(projection_error, axis=0, x_slice=None)
+
+    # def projection_out_error(self, y_domain=(-0.1, 0.1), z_domain=(-0.1, 0.1)):
+    #     s = self.ratio.get_slice(None, y_domain, z_domain)
+    #     _, y_domain, z_domain = s
+    #     data = self.ratio.errors[:,y_domain,z_domain]
+    #     sum = np.sqrt((data ** 2).sum(axis=(1, 2)))
+    #     # sum = sum / (s[1].stop - s[1].start) / (s[2].stop - s[2].start)
+    #     return sum
+
+    # def projection_side(self, x_domain=(-0.1, 0.1), z_domain=(-0.1, 0.1)):
+    #     """
+    #     Returns a numpy array of the entire 'side' axis, with the other two
+    #     axes restricted by the parameters
+    #     """
+    #     data = self.ratio[x_domain,:,z_domain]
+    #     sum = data.sum(axis=(0, 2))
+    #     return sum
+
+    def projection_side_error(self, x_domain=(-0.1, 0.1), z_domain=(-0.1, 0.1)):
+        x_domain, _, z_domain = self.ratio.get_slice(x_domain, None, z_domain)
+        data = self.ratio.errors[x_domain,:,z_domain]
         sum = data.sum(axis=(0, 2))
         return sum
 
-    def projection_long(self, x_domain=(-0.1, 0.1), y_domain=(-0.1, 0.1)):
-        data = self.ratio[x_domain,y_domain,:]
-        sum = data.sum(axis=(1, 2))
+    # def projection_long(self, x_domain=(-0.1, 0.1), y_domain=(-0.1, 0.1)):
+    #     data = self.ratio[x_domain,y_domain,:]
+    #     sum = data.sum(axis=(0, 1))
+    #     return sum
+
+    def projection_long_error(self, x_domain=(-0.1, 0.1), y_domain=(-0.1, 0.1)):
+        x_domain, y_domain, _ = self.ratio.get_slice(x_domain, y_domain, None)
+        data = self.ratio.errors[x_domain, y_domain, :]
+        sum = data.sum(axis=(0, 1))
         return sum
 
     def get_projection(self, x_slice, y_slice, z_slice, summed_axes):
