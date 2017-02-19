9482952b1fddb49c6abe73e1f48878df5c5636b0
Andrew Kubera <andrew.michael.kubera@cern.ch>
2017-02-19 13:10:16 -0500

    3d_fit: Added plotting code for 3D fit. Updated 1D fit to use new stumpy.Histogram objects

--- post_analysis/3d_fit.py
+++ post_analysis/3d_fit.py
@@ -38,7 +38,7 @@ from ROOT import (
     TObjString,
     TGraph,
 )
-import root_numpy
+
 print("Done (%fs)" % (time.monotonic() - TSTART), file=sys.stderr)
 
 CACHE = []
@@ -160,10 +160,11 @@ def do_qinv_analysis(num, den, cf_title="CF; q (GeV); CF(q)", output_imagename='
     # use old 'simple' residual method
     ratio = num / den
     ratio, errors = ratio[fit_slice], ratio.errors[fit_slice]
-    qinv_fit = minimize(ModelClass.as_resid, qinv_params, args=(x, ratio, errors))
+    assert ratio.shape == errors.shape, 'Shapes do not match (%s ≠ %s)' % (ratio.shape, errors.shape) # '({ratio.shape} ≠ {errors.shape})'
+    # qinv_fit = minimize(ModelClass.as_resid, qinv_params, args=(x, ratio, errors))
 
     # random data to check chi^2
-    # rdata = GaussianModel().eval(qinv_params, x=x)
+    # rdata = GaussianMSodel().eval(qinv_params, x=x)
     # offset = np.random.normal(0, 0.01, len(rdata))
     # rdata += offset + 0.001
     # errors = np.abs(offset)  # np.random.normal(0, 0.01, len(rdata))
@@ -240,54 +241,66 @@ for analysis in femtolist:
     #
     # Qinv
     #
-    # if False:
-    num, den = analysis.qinv_pair
+    if True:
+        # num, dern = analysis.qinv_pair
+        n = get_root_object(analysis._data, analysis.QINV_NUM_PATH)
+        if n == None:
+            print("Error! Could not load numerator in analysis")
+            continue
+        num = Histogram.BuildFromRootHist(n)
 
-    if num == None or den == None:
-        print("Missing Qinv plots")
-        print('  num:', num)
-        print('  den:', den)
-    else:
-        print(" ***** Q_inv Study *****\n")
-        qinv_title = make_cf_title(title='Q_{inv}', units='q_{inv}')
-        do_qinv_analysis(num, den, cf_title=qinv_title, output_imagename=analysis_name + '_qinv.eps')
+        d = get_root_object(analysis._data, analysis.QINV_DEN_PATH)
+        if d == None:
+            print("Error! Could not load denominator in analysis")
+            continue
+        den = Histogram.BuildFromRootHist(d)
+
+        if num == None or den == None:
+            print("Missing Qinv plots")
+            print('  num:', num)
+            print('  den:', den)
+        else:
+            print(" ***** Q_inv Study *****\n")
+            qinv_title = make_cf_title(title='Q_{inv}', units='q_{inv}')
+            do_qinv_analysis(num, den, cf_title=qinv_title, output_imagename=analysis_name + '_qinv.eps')
 
     #
     # Fake Qinv - Kt binned
     #
-    fake_qinv_num = get_root_object(analysis, ["fakeNum_q3D_pip"])
-    fake_qinv_den = get_root_object(analysis, ["FakeDen_q3D_pip"])
-    if fake_qinv_num == None or fake_qinv_den == None:
-        print("Missing fake_qinv", fake_qinv_num, fake_qinv_den)
-    else:
-        print("\n\n")
-        print(" ***** Fake Q_inv Study *****\n")
-        do_qinv_analysis(fake_qinv_num, fake_qinv_den,
-                         cf_title="Fake Q_inv; q_{inv} (fake); C(q_{inv});",
-                         output_imagename=analysis_name + '_fake_qinv.png')
+    # fake_qinv_num = get_root_object(analysis, ["fakeNum_q3D_pip"])
+    # fake_qinv_den = get_root_object(analysis, ["FakeDen_q3D_pip"])
+    # if fake_qinv_num == None or fake_qinv_den == None:
+    #     print("Missing fake_qinv", fake_qinv_num, fake_qinv_den)
+    # else:
+    #     print("\n\n")
+    #     print(" ***** Fake Q_inv Study *****\n")
+    #     do_qinv_analysis(fake_qinv_num, fake_qinv_den,
+    #                      cf_title="Fake Q_inv; q_{inv} (fake); C(q_{inv});",
+    #                      output_imagename=analysis_name + '_fake_qinv.png')
 
     #
     # Qinv - Kt binned
     #
-    kt_qinv = get_root_object(analysis, ["KT_Qinv"])
-    if kt_qinv != None:
-        print("   Found kt binned Qinv")
-        for ktbin in kt_qinv:
-            kt_bin_name = ktbin.GetName()
-            print("       (%s)" % kt_bin_name, NUM_QINV_PATH)
-            ktn = get_root_object(ktbin, NUM_QINV_PATH)
-            ktd = get_root_object(ktbin, DEN_QINV_PATH)
-            if ktn == None:
-                print("Could not find numerator")
-                continue
-            if ktd == None:
-                print("Could not find denominator")
-                continue
-            do_qinv_analysis(ktn, ktd,
-                            cf_title="KT %s Q_inv; q_{inv} (fake); C(q_{inv});" % (kt_bin_name),
-                            output_imagename=analysis_name + '_fake_qinv.png')
-    # input()
-    # continue
+    if False:
+        kt_qinv = get_root_object(analysis, ["KT_Qinv"])
+        if kt_qinv != None:
+            print("   Found kt binned Qinv")
+            for ktbin in kt_qinv:
+                kt_bin_name = ktbin.GetName()
+                print("       (%s)" % kt_bin_name, NUM_QINV_PATH)
+                ktn_root = get_root_object(ktbin, NUM_QINV_PATH)
+                if ktn_root == None:
+                    print("Could not find numerator")
+                    continue
+                ktn = Histogram.BuildFromRootHist(ktn_root)
+                ktd_root = get_root_object(ktbin, DEN_QINV_PATH)
+                if ktd_root == None:
+                    print("Could not find denominator")
+                    continue
+                ktd = Histogram.BuildFromRootHist(ktd_root)
+                do_qinv_analysis(ktn, ktd,
+                                cf_title="KT %s Q_inv; q_{inv} (fake); C(q_{inv});" % (kt_bin_name),
+                                output_imagename=analysis_name + '_fake_qinv.png')
 
     #
     # 3D
@@ -313,16 +326,18 @@ for analysis in femtolist:
 
     hist_3d = Q3D(q3d_num, q3d_den)
 
-    domains_ranges = (-.05, 0.05), (-.05, 0.05), (-.05, 0.05)
-    xx, yy, zz = np.meshgrid(*(hist_3d.num.axes[i].bin_centers for i in range(3)))
+    domains_ranges = (-0.01, 0.01), (-0.01, 0.01), (-0.0, 0.007) #1, 0.01)
+    # domains_ranges = (-.09, 0.09), (-.09, 0.09), (-.09, 0.09)
+    xx, yy, zz = np.meshgrid(*(a.bin_centers for a in hist_3d.num.axes))
     q_domain = np.stack([xx.flatten(), yy.flatten(), zz.flatten()]).T
     ratio = hist_3d.ratio.data.flatten()
     ratio_err = hist_3d.ratio.errors.flatten()
     print('q_domain', q_domain.shape, 'ratio', ratio.shape, ratio_err.shape)
+
     # domains_ranges = (1, -2), (1, -2), (1, -1)
     # slices = hist_3d.num.get_slice(*domains_ranges)
     # slices = slice(None), slice(None), slice(None)
-    fitfunc_3d(q3d_params, q_domain, (ratio, ratio_err))
+    # fitfunc_3d(q3d_params, q_domain, (ratio, ratio_err))
     print("------")
     t = time.monotonic()
     TIMESTART = time.monotonic()
@@ -330,17 +345,92 @@ for analysis in femtolist:
     print(":::::", fit_res)
     TIME_DELTA = time.monotonic() - TIMESTART
     report_fit(fit_res)
-    print("fitting time %0.3fss (%0.3f ms/call)" % (TIME_DELTA, TIME_DELTA * 1e3 / fit_res.nfev))
-    qout = hist_3d.projection_out((-0.05, 0.05), (-0.05, 0.05))
-    qlong = hist_3d.projection_long((-0.05, 0.05), (-0.05, 0.05))
-    print(qlong)
+    print("fitting time %0.3fs (%0.3f ms/call)" % (TIME_DELTA, TIME_DELTA * 1e3 / fit_res.nfev))
+
+    fit_domain = q_domain
+    best_fit = fitfunc_3d(fit_res.params, fit_domain).reshape(hist_3d.num.shape)
+
+    # slices
+    s = hist_3d.num.get_slice(*domains_ranges)
+
+    x_width = s[0].stop - s[0].start
+    y_width = s[1].stop - s[1].start
+    z_width = s[2].stop - s[2].start
+    print(':>', x_width, y_width, z_width)
+    # x_width = y_width = z_width = 1.8 # 0.05 + 0.05 # hist_3d.num.x_axis[-0.05]
+
+    print(">>", s)
+
+    qout = hist_3d.projection_out(y_slice=domains_ranges[1], z_slice=domains_ranges[2]) # / y_width / z_width / fit_res.params['norm']
+    qout_err = hist_3d.projection_out_error(y_slice=domains_ranges[1], z_slice=domains_ranges[2]) # / y_width / z_width
+    fit_qout = best_fit[:, s[1], s[2]].sum(axis=(1, 2)) / y_width / z_width # / fit_res.params['norm']
+
+    qside = hist_3d.projection_side(x_slice=domains_ranges[0], z_slice=domains_ranges[2]) / x_width / z_width
+    qside_err = hist_3d.projection_side_error(*domains_ranges[:2])
+    fit_qside = best_fit[s[0], :, s[2]].sum(axis=(0, 2)) / x_width / z_width
+
+    qlong = hist_3d.projection_long(x_slice=domains_ranges[0], y_slice=domains_ranges[1])
+    qlong_err = hist_3d.projection_long_error(*domains_ranges[:2])
+    fit_qlong = best_fit[s[0], s[1], :].sum(axis=(0, 1)) / x_width / y_width
+
+    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2) # , sharex='col', sharey='row')
+
+    qout_xaxis = hist_3d.ratio.x_axis.bin_centers
+    print(qout.shape, qout_err.shape, qout_xaxis.shape)
+    ax1.errorbar(qout_xaxis, qout, fmt='.', yerr=qout_err)
+    # ax1.plot(qout_xaxis, fit_qout)
+    ax1.set_title('Qout')
+
+    qside_xaxis = hist_3d.ratio.y_axis.bin_centers
+    ax2.errorbar(qside_xaxis, qside, fmt='.', yerr=qside_err)
+    ax2.plot(qside_xaxis, fit_qlong)
+    ax2.set_title('Qside')
+
+    qlong_xaxis = hist_3d.ratio.z_axis.bin_centers
+    # ax3.errorbar(qlong_xaxis, qlong, fmt='.', yerr=qlong_err)
+    ax3.plot(qlong_xaxis, fit_qlong)
+    ax3.set_title('Qlong')
+
+    ax4.set_axis_off()
+    ax4.text(0.2, 0.5,
+        '$R_{{out}}$: {r_out.value:03f} ≠ {r_out.stderr:.03f}\n'
+        '$R_{{side}}$: {r_side.value:03f}\n'
+        '$R_{{long}}$: {r_long.value:03f}\n'
+        '\n'
+        '$\lambda$: {lam.value:03f}\n'
+        '$norm$: {norm.value:03f}\n'.format(
+            **fit_res.params),
+        fontsize=20,
+        horizontalalignment='left',
+        verticalalignment='center',
+        transform=ax4.transAxes)
+
+    ax4.text(0.5, 0.0, 'run2, 0-5% centrality')
+    # ax4.text(3, 8, 'boxed italics text in data coords', style='italic',
+    #     bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
+    # ax3.scatter(x, 2 * y ** 2 - 1, color='r')
+    # ax4.plot(x, 2 * y ** 2 - 1, color='r')
+    plt.tight_layout()
+    plt.show()
+    # plt.figure()
+    #
+    # plt.
+    # plt.
+    # plt.show()
+
+    qlong = hist_3d.projection_long(*domains_ranges[:2])
+    # print(qlong)
 
 
     kt_binned = analysis['KT_Q3D']
     for kt_bin in kt_binned:
-        print(kt_bin)
+        kt_bin_name = kt_bin.GetName()
+        print(':::::', kt_bin_name)
         q3d_num = get_root_object(kt_bin, NUM_Q3D_PATH)
         q3d_den = get_root_object(kt_bin, DEN_Q3D_PATH)
+        if q3d_num == None or q3d_den == None:
+            print("  Could not find num/den pair")
+            continue
         hist_3d = Q3D(q3d_num, q3d_den)
 
         fit_res = minimize(fitfunc_3d, q3d_params, args=(q_domain, (ratio, ratio_err)))
